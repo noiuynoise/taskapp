a4b269c8f76d885d1c28dc990669011db135b826
silverywater <catgirl2359@gmail.com>
2021-11-21 19:40:35 -0500

    ProjectRequest

--- taskapp/src/main/java/com/cs3733/taskapp/http/ProjectRequest.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/ProjectRequest.java
@@ -1,5 +1,22 @@
 package com.cs3733.taskapp.http;
 
 public class ProjectRequest {
-
+	
+	String projectID;
+	
+	public String getProjectID () {return this.projectID;}
+	public void setProjectID (String projectID) {this.projectID = projectID;}
+	
+	
+	public ProjectRequest (String projectID)
+	{
+		this.projectID = projectID;
+	}
+	
+	public ProjectRequest ()
+	{
+		
+	}
+	
+	
 }
