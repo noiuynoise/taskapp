b4b904094b23e8e913b51f7926eb326e9434e026
Ezekiel Andreassen <zeke2856@gmail.com>
2021-11-21 19:39:29 -0500

    AssignTeammateRequest  done

--- taskapp/src/main/java/com/cs3733/taskapp/http/AssignTeammateRequest.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/AssignTeammateRequest.java
@@ -1,5 +1,30 @@
 package com.cs3733.taskapp.http;
 
 public class AssignTeammateRequest {
+	String projectid;
+	String taskid;
+	Boolean status;
 
+	
+	
+	public String getProjectID() { return projectid; }
+	public void setProjectID(String projectid) { this.projectid = projectid; }
+
+	public String getTaskID() { return taskid; }
+	public void setTaskID(String taskid) { this.taskid = taskid; }
+	
+	public String toString() {
+		return "AssignTeammateRequest(" + projectid + "," + taskid +  "," + status + ")";
+	}
+	
+	public AssignTeammateRequest(String projectid, String taskid, Boolean status) {
+		this.projectid = projectid;
+		this.taskid = taskid;
+		this.status = status;
+	}
+	
+	public AssignTeammateRequest() {
+	}
 }
+
+
