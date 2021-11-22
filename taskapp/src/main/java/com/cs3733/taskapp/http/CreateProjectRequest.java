07c54b8e25ef00df7162f0e55c826a600b658cd9
Ezekiel Andreassen <zeke2856@gmail.com>
2021-11-21 19:46:24 -0500

    createprojectrequest done

--- taskapp/src/main/java/com/cs3733/taskapp/http/CreateProjectRequest.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/CreateProjectRequest.java
@@ -1,5 +1,22 @@
 package com.cs3733.taskapp.http;
 
 public class CreateProjectRequest {
+	String projectid;
 
+	
+	public String getProjectID() { return projectid; }
+	public void setProjectID(String projectid) { this.projectid = projectid; }
+
+	public String toString() {
+		return "CreateProjectRequest(" + projectid + ")";
+	}
+	
+	public CreateProjectRequest(String projectid) {
+		this.projectid = projectid;
+	}
+	
+	public CreateProjectRequest() {
+	}
 }
+
+
