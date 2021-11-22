ace99196e11da9f69892052e3950df71453d8116
silverywater <catgirl2359@gmail.com>
2021-11-21 19:35:43 -0500

    deleting comments

--- taskapp/src/main/java/com/cs3733/taskapp/http/Task.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/Task.java
@@ -1,23 +1,6 @@
 package com.cs3733.taskapp.http;
 
 public class Task {
-
-//	Task:
-//	    type: object
-//	    required:
-//	    - name
-//	    properties:
-//	      name:
-//	        type: string
-//	      subtasks:
-//	        type: array
-//	        items: 
-//	          $ref: "#/definitions/Task"
-//	      id:
-//	        type: string
-//	      complete:
-//	        type: boolean
-	
 	
 	String name;
 	Task subtasks[];
