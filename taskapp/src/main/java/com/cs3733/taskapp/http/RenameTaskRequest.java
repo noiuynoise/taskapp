811a577cf6982407668172b20abffbe5b203c416
silverywater <catgirl2359@gmail.com>
2021-11-21 19:34:17 -0500

    RenameTaskRequest

--- taskapp/src/main/java/com/cs3733/taskapp/http/RenameTaskRequest.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/RenameTaskRequest.java
@@ -2,4 +2,44 @@ package com.cs3733.taskapp.http;
 
 public class RenameTaskRequest {
 
+//	RenameTaskRequest:
+//	    type: object
+//	    required:
+//	    - taskid
+//	    - projectid
+//	    - newname
+//	    properties:
+//	      taskid:
+//	        type: string
+//	      projectid:
+//	        type: string
+//	      newname:
+//	        type: string
+	
+	
+	String taskID;
+	String projectID;
+	String newName;
+	
+	public String getTaskID () {return this.taskID;}
+	public void setTaskID (String taskID) {this.taskID = taskID;}
+	
+	public String getProjectID () {return this.projectID;}
+	public void setProjectID (String projectID) {this.projectID = projectID;}
+	
+	public String getNewName () {return this.newName;}
+	public void setNewName (String newName) {this.newName = newName;}
+	
+	public RenameTaskRequest (String taskID, String projectID, String newName) {
+		this.taskID = taskID;
+		this.projectID = projectID;
+		this.newName = newName;
+	}
+	
+	public RenameTaskRequest () {
+		
+	}
+	
+	
+	
 }
