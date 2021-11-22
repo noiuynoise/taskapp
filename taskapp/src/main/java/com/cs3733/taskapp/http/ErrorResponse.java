82ef42e50d3185ea67400a257fad65112d813411
Ezekiel Andreassen <zeke2856@gmail.com>
2021-11-21 19:34:52 -0500

    ErrorResponse done

--- taskapp/src/main/java/com/cs3733/taskapp/http/ErrorResponse.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/ErrorResponse.java
@@ -1,5 +1,18 @@
 package com.cs3733.taskapp.http;
 
 public class ErrorResponse {
-
+	String errorMessage;
+	
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage= errorMessage;
+	}
+	
+	public String toString() {
+		return errorMessage;
+	}
+	
+	
+	public String ErrorResponse() {
+		return errorMessage;
+	}
 }
