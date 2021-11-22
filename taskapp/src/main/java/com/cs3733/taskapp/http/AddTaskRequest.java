5bce10d9cb50b6f0a46c46de0f985078f2ef955f
Ezekiel Andreassen <zeke2856@gmail.com>
2021-11-21 19:23:30 -0500

    AddTaskRequest  Done

--- taskapp/src/main/java/com/cs3733/taskapp/http/AddTaskRequest.java
+++ taskapp/src/main/java/com/cs3733/taskapp/http/AddTaskRequest.java
@@ -1,5 +1,25 @@
 package com.cs3733.taskapp.http;
 
 public class AddTaskRequest {
+	String projectid;
+	Task tasks[];
 
+	public Task[] getTasks() { return tasks; }
+	public void setTasks(Task[] tasks) { this.tasks = tasks; }
+	
+	public String getProjectID() { return projectid; }
+	public void setProjectID(String projectid) { this.projectid = projectid; }
+
+	public String toString() {
+		return "TeamViewResponse(" + projectid + "," + tasks + ")";
+	}
+	
+	public AddTaskRequest(Task[] tasks, String projectid) {
+		this.tasks = tasks;
+		this.projectid = projectid;
+	}
+	
+	public AddTaskRequest() {
+	}
 }
+
