d6de0939705d2f6ba6763463a703bb631938f521
Colb131 <cfrechette@wpi.edu>
2021-12-01 14:32:50 -0500

    Fixed shitty circular code

--- FrontEnd/api.js
+++
@@ -1,7 +0,0 @@
-// all access driven through BASE. Must end with a SLASH
-// be sure you change to accommodate your specific API Gateway entry point
-var base_url = "https://5tcgfaucil.execute-api.us-east-2.amazonaws.com/Beta/";
-
-var add_url    = base_url + "createproject";   // POST
-var project_view_url    = base_url + "projectview/";   // GET
-var admin_view_url = base_url + "listprojects" //GET
--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -1,9 +1,7 @@
 // all access driven through BASE. Must end with a SLASH
 // be sure you change to accommodate your specific API Gateway entry point
-var base_url = "https://xqagxkd8b7.execute-api.us-east-1.amazonaws.com/beta/";
+var base_url = "https://5tcgfaucil.execute-api.us-east-2.amazonaws.com/Beta/";
 
-var add_url    = base_url + "calculator";   // POST
-var list_url   = base_url + "constants";    // GET
-var create_url = base_url + "constants";    // POST
-
-var delete_url = base_url + "constants";    // POST with {name}   [challenge in getting DELETE to work]
+var add_url    = base_url + "createproject";   // POST
+var project_view_url    = base_url + "projectview/";   // GET
+var admin_view_url = base_url + "listprojects" //GET
