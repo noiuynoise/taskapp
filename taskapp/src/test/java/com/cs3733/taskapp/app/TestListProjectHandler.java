5b572cd9e518f303b6c41d2841a6593d33de0100
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2021-12-17 01:43:29 -0500

    Update TestListProjectHandler.java

--- taskapp/src/test/java/com/cs3733/taskapp/app/TestListProjectHandler.java
+++ taskapp/src/test/java/com/cs3733/taskapp/app/TestListProjectHandler.java
@@ -1,7 +1,89 @@
 package com.cs3733.taskapp.app;
 
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.cs3733.taskapp.http.AddTaskRequest;
+import com.cs3733.taskapp.http.CreateProjectRequest;
+import com.cs3733.taskapp.http.DeleteProjectRequest;
+import com.cs3733.taskapp.http.ListProjectsResponse;
+import com.cs3733.taskapp.http.Project;
+import com.cs3733.taskapp.http.ProjectRequest;
+import com.cs3733.taskapp.http.ProjectResponse;
+import com.cs3733.taskapp.http.Task;
+
 import junit.framework.TestCase;
 
-public class TestListProjectHandler extends TestCase {
+@RunWith(MockitoJUnitRunner.class)
+public class TestListProjectHandler extends LambdaTest {
+    private final String CONTENT_TYPE = "image/jpeg";
+    private S3Event event;
 
+    @Mock
+    private AmazonS3 s3Client;
+    @Mock
+    private S3Object s3Object;
+
+    @Captor
+    private ArgumentCaptor<GetObjectRequest> getObjectRequest;
+
+    @Before
+    public void setUp() throws IOException {
+        event = TestUtils.parse("/s3-event.put.json", S3Event.class);
+
+        // TODO: customize your mock logic for s3 client
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(CONTENT_TYPE);
+        when(s3Object.getObjectMetadata()).thenReturn(objectMetadata);
+        when(s3Client.getObject(getObjectRequest.capture())).thenReturn(s3Object);
+    }
+
+    private Context createContext() {
+        TestContext ctx = new TestContext();
+
+        // TODO: customize your context here if needed.
+        ctx.setFunctionName("Your Function Name");
+
+        return ctx;
+    }
+
+	void testSuccessInput() throws IOException {
+
+    	
+    }
+
+	@Test
+   	public void testListProjectRequest() {
+    	//create project to check if in list
+    	CreateProjectHandler handler1 = new CreateProjectHandler(s3Client);
+    	String testProjectName = "teusdhgoirtslngi";
+    	ProjectResponse response1 = handler1.handleRequest(testProjectName, createContext());
+    	//is it in the list?
+    	ListProjectsHandler handler2 = new ListProjectsHandler(s3Client);
+    	ListProjectsResponse response2 = handler2.handleRequest(true, createContext());
+
+    	boolean work = false;
+    	for(Project p: response2.getProjects()) {
+    		if(p.getProjectID().equals(response1.getProjectTUUID()))
+    			work = true;
+    	}
+    	Assert.assertTrue(work);
+   	}
+	
 }
