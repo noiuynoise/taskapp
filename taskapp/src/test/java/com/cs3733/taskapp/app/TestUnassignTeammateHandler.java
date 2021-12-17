a2cfb41d972184b3fd18ca1a386a36d3c3a8d73f
zeke2856 <46726503+zeke2856@users.noreply.github.com>
2021-12-17 05:01:57 -0500

    Update TestUnassignTeammateHandler.java

--- taskapp/src/test/java/com/cs3733/taskapp/app/TestUnassignTeammateHandler.java
+++ taskapp/src/test/java/com/cs3733/taskapp/app/TestUnassignTeammateHandler.java
@@ -1,7 +1,92 @@
 package com.cs3733.taskapp.app;
 
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.cs3733.taskapp.http.AddTaskRequest;
+import com.cs3733.taskapp.http.DeleteProjectRequest;
+import com.cs3733.taskapp.http.ProjectResponse;
+import com.cs3733.taskapp.http.Task;
+import com.cs3733.taskapp.http.Teammate;
+import com.cs3733.taskapp.http.TeammateRequest;
+
 import junit.framework.TestCase;
 
-public class TestUnassignTeammateHandler extends TestCase {
+@RunWith(MockitoJUnitRunner.class)
+public class TestUnassignTeammateHandler extends LambdaTest {
+    private final String CONTENT_TYPE = "image/jpeg";
+    private S3Event event;
 
+    @Mock
+    private AmazonS3 s3Client;
+    @Mock
+    private S3Object s3Object;
+
+    @Captor
+    private ArgumentCaptor<GetObjectRequest> getObjectRequest;
+
+
+    private Context createContext() {
+        TestContext ctx = new TestContext();
+
+        // TODO: customize your context here if needed.
+        ctx.setFunctionName("Your Function Name");
+
+        return ctx;
+    }
+
+	void testSuccessInput() throws IOException {
+
+    	
+    }
+	
+    @Test
+	public void testSuccessAddedTeammate() {
+    	//create project to delete
+    	System.out.println("TestUnassignTeammateHandler");
+    	CreateProjectHandler handler1 = new CreateProjectHandler(s3Client);
+    	String testProjectName = "123 TestUnassignTeammateHandler";
+    	ProjectResponse response1 = handler1.handleRequest(testProjectName, createContext());
+  
+    	AddTeammateHandler handler = new AddTeammateHandler(s3Client);
+    	    	
+    	String testPUUID = response1.getProjectTUUID();
+    	
+    	String nm = "jnksdijngsrojn";
+
+    	TeammateRequest req = new TeammateRequest(testPUUID, nm);
+        Teammate response = handler.handleRequest(req, createContext());
+        
+        Assert.assertTrue(response.getName().equals(nm));
+        
+        UnassignTeammateHandler handler5 = new UnassignTeammateHandler(s3Client);
+        Boolean isRemoved = handler5.handleRequest(req, null);
+        
+        Assert.assertTrue(isRemoved);
+      	
+        //delete it
+    	DeleteProjectHandler handler2 = new DeleteProjectHandler(s3Client);
+    	DeleteProjectRequest req2 = new DeleteProjectRequest(testPUUID);
+    	Boolean response2 = handler2.handleRequest(testPUUID, createContext());
+    	
+    }
+	
+	
 }
+
